da00a22f7eb0db2b855a6d84cca0e5fdd5d1b5bf
MathieuWILLEM <98036973+MathieuWILLEM@users.noreply.github.com>
2025-01-08 15:03:31 +0100

    Document

---
+++ src/main/java/fr/efrei/springrag/domain/Document.java
@@ -0,0 +1,14 @@
+package fr.efrei.springrag.domain;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Document {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    private String title;
+}
