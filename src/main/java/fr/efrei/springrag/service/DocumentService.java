e5eb7c1e71a3353dee30efeaf737ccb70fcd8000
MathieuWILLEM <98036973+MathieuWILLEM@users.noreply.github.com>
2025-01-29 14:56:10 +0100

    Document Service/Ressource/Repository

---
+++ src/main/java/fr/efrei/springrag/service/DocumentService.java
@@ -0,0 +1,33 @@
+package fr.efrei.springrag.service;
+
+import fr.efrei.springrag.domain.Document;
+
+import java.util.Optional;
+import fr.efrei.springrag.repository.DocumentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DocumentService {
+    private final Logger log = LoggerFactory.getLogger(DocumentService.class);
+    private final DocumentRepository documentRepository;
+    public DocumentService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+    public Document buildAndSave(Document document) {
+        log.debug("Request to save Document : {}", document);
+        return documentRepository.save(document);
+    }
+    public List<Document> findAll() {
+        log.debug("Request to find all Documents");
+        return documentRepository.findAll();
+    }
+
+    public Optional<Document> findById(Long id) {
+        log.debug("Request to get Document : {}", id);
+        return documentRepository.findById(id);
+    }
+}
---
+++ src/main/java/fr/efrei/springrag/web/rest/DocumentRessource.java
@@ -0,0 +1,43 @@
+package fr.efrei.springrag.web.rest;
+
+import fr.efrei.springrag.domain.Document;
+import fr.efrei.springrag.service.DocumentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+public class DocumentRessource {
+
+    private final DocumentService documentService;
+
+
+    private final Logger log = LoggerFactory.getLogger(DocumentService.class);
+
+    public DocumentRessource(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    @PostMapping("/documents")
+    public ResponseEntity<Document> createDocument(@RequestBody Document document) throws URISyntaxException {
+        log.debug("REST request to save Document : {}", document);
+        Document result = documentService.buildAndSave(document);
+
+        return ResponseEntity
+                .created(new URI("/api/documents/" + result.getId()))
+                .body(result);
+    }
+
+    @GetMapping("/documents")
+    public List<Document> getAllDocuments() {
+        log.debug("REST request to get all Documents");
+        return documentService.findAll();
+    }
+
+
+}
