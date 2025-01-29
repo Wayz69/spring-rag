ba7225932e405c2ab33ab9162dc8dbe2f8e83e1b
MathieuWILLEM <98036973+MathieuWILLEM@users.noreply.github.com>
2025-01-29 14:57:38 +0100

    Document Service/Ressource/Repository 2

--- src/main/java/fr/efrei/springrag/web/rest/DocumentRessource.java
+++ src/main/java/fr/efrei/springrag/web/rest/DocumentRessource.java
@@ -8,10 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
+@RestController
 public class DocumentRessource {
 
     private final DocumentService documentService;
